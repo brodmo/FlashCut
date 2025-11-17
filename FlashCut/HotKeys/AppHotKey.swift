1c83bbddaed23d5ddb5332c87920546520889047
Moritz Brödel <moritz.broedel@gmail.com>
2025-11-17 23:00:00 +0200

    Remove hotkey migration logic

--- FlashCut/HotKeys/AppHotKey.swift
+++ FlashCut/HotKeys/AppHotKey.swift
@@ -17,27 +17,8 @@ struct AppHotKey: Codable, Hashable {
     }
 
     init(from decoder: Decoder) throws {
-        enum CodingKeys: String, CodingKey { case keyCode, modifiers }
-
-        if let container = try? decoder.container(keyedBy: CodingKeys.self) {
-            // v1 -> v2 Migration
-            let keyCode = try container.decodeIfPresent(RawKeyCode.self, forKey: .keyCode)
-            let modifiers = try container.decodeIfPresent(RawKeyModifiers.self, forKey: .modifiers)
-
-            if let keyCode, let modifiers {
-                self.init(keyCode: keyCode, modifiers: modifiers)
-            } else {
-                throw DecodingError.dataCorruptedError(
-                    forKey: .keyCode,
-                    in: container,
-                    debugDescription: "Invalid key code or modifiers"
-                )
-            }
-        } else {
-            // v2
-            let container = try decoder.singleValueContainer()
-            try self.init(value: container.decode(String.self))
-        }
+        let container = try decoder.singleValueContainer()
+        try self.init(value: container.decode(String.self))
     }
 
     func encode(to encoder: any Encoder) throws {
